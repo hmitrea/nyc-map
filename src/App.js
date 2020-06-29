65cc8364143f09895047bccc812b752a6daf06f7
ubuntu <mail@adnanwahab.com>
2020-06-28 20:29:07 -0400

    deploy

--- src/App.js
+++ src/App.js
@@ -5,13 +5,10 @@ import _ from 'lodash'
 
 import { LayerControls, MapStylePicker, HEXAGON_CONTROLS } from './components/controls'
 import ListView from './components/list-view.js'
-import Charts from './components/charts'
-import LoadLayers from './layers/index'
-import DelayedPointLayer from './layers/DelayedPointLayer'
-
-const layers = LoadLayers.layers
-require('./style.css')
 import {H3HexagonLayer} from '@deck.gl/geo-layers';
+import {HeatmapLayer} from '@deck.gl/aggregation-layers';
+
+require('./style.css')
 
 const INITIAL_VIEW_STATE = {
   longitude: -73.91922208269459,
@@ -27,47 +24,18 @@ const INITIAL_VIEW_STATE = {
 const MAPBOX_TOKEN = // process.env.MapboxAccessToken; // eslint-disable-line
 'pk.eyJ1IjoiYXdhaGFiIiwiYSI6ImNpenExZHF0ZTAxMXYzMm40cWRxZXY1d3IifQ.TdYuekJQSG1eh6dDpywTxQ'
 
-const choice = layers[_.random(layers.length)]
-
-const makePoints = () => {
-  return {
-    position: [-73.91922208269459, 40.72185277744134],
-    hour: new Date().getUTCHours(),
-    pickup: true
-  }
-}
 
 function Root () {
   const [selection, setSelection] = useState('311-Blocked-Driveway')
 
   const [data, setData] = useState({
-    selectedIndex: choice
+    selectedIndex: 0
   })
 
-  useEffect(() => {
-    const fetchData = async () => {
-      LoadLayers.load().then(setData)
-    }
-    fetchData()
-  }, [])
 
-  const _onSelect = () => {}
-
-  const _onHighlight = () => {}
-
-  const _onStyleChange = (style) => {
-    setData({ style })
-  }
   const _onWebGLInitialize = (gl) => {
     gl.enable(gl.DEPTH_TEST)
     gl.depthFunc(gl.LEQUAL)
-  }
-
-  const _onClick = (info) => {
-    if (info.object) {
-      // eslint-disable-next-line
-      alert(`${info.object.properties.name} (${info.object.properties.abbrev})`);
-    }
   }
 
   const _updateLayerSettings = (settings) => {
@@ -75,7 +43,6 @@ function Root () {
   }
 
   const _selectLayer = (e) => {
-
     setSelection( e.target.id )
   }
 
